fef601e825d0da0c8d0ef62b10a0e2c556d66583
EinsteinYap <einsteinyap2014@gmail.com>
2023-10-30 00:14:32 +0800

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,29 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'co.dcex.decxApp',
   appName: 'DCEX-MARKET',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
-  }
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      // launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#30303e",
+      // androidSplashResourceName: "splash",
+      // androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "small",
+      iosSpinnerStyle: "small",
+      // spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true
+
+    },
+  },
 };
 
 export default config;
